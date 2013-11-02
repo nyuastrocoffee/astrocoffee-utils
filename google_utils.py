3a01fc8e8e4934fae8c18ca5b512855a0705a886
AstroCoffee <astrocoffee@astro.physics.nyu.edu>
2013-11-02 14:16:16 -0400

    add google calendar retrieve

--- google_utils.py
+++ google_utils.py
@@ -1,6 +1,31 @@
+import time
 import string
 import gspread
 import numpy as np
+import gdata.calendar.client
+
+def get_calender_events(username, password):
+    """
+    Get list of astrocoffee events, return next two dates
+    """
+    cc = gdata.calendar.client.CalendarClient(source='')
+    cc.ClientLogin('nyuastrocoffee@gmail.com', 'nyuccpp2012', source='')
+
+    today = time.strftime('%Y-%m-%d') 
+    end_date = '-'.join([str(int(time.strftime('%Y'))+ 1),
+                         time.strftime('%m-%d')])
+    query = gdata.calendar.client.CalendarEventQuery(start_min=today, 
+                                                     start_max=end_date)
+    feed = cc.GetCalendarEventFeed(q=query)
+    for i, event in zip(xrange(len(feed.entry)), feed.entry):
+        if ((event.title.text == 'Astrocoffee') | \
+                (event.title.text == 'Astrocoffee')):    
+            events = [w.start[:10] for w in event.when]
+
+    if events[0] == today:
+        events = events[1:3]
+
+    return events[:2]
 
 def fetch_from_google_docs(username, password, docname):
     """
@@ -59,3 +84,5 @@ if __name__ == '__main__':
     data, k = fetch_from_google_docs(u, p, d)
     
     insert_into_google_docs(u, p, d, data)
+
+    print get_calender_events(u, p)
