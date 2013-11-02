f3c439bc71624c29570151b8ffc9c0e5020c9208
AstroCoffee <astrocoffee@astro.physics.nyu.edu>
2013-11-02 13:00:24 -0400

    working upload download

--- google_utils.py
+++ google_utils.py
@@ -1,3 +1,4 @@
+import string
 import gspread
 import numpy as np
 
@@ -25,7 +26,27 @@ def insert_into_google_docs(username, password, docname, data):
     """
     # connect
     gc = gspread.login(username + '@gmail.com', password)
-    ws = gc.open(docname)
+    ws = gc.open(docname).sheet1
+
+    # flattened list
+    keys = ['name', 'email', 'last-presented', 'gone-until', 'going-on']
+    db = [keys]
+    Nrows = len(data[keys[0]])
+    for i in range(Nrows):
+        db.append([data[k][i] for k in keys])
+    db = np.array(db).ravel()
+
+    # list of cells
+    end_letter = string.ascii_uppercase[len(keys) - 1]
+    end_number = str(Nrows + 1)
+    cell_list = ws.range(''.join(['A1:', end_letter, end_number]))
+
+    # fill in cells
+    for i, c in enumerate(cell_list):
+        c.value = db[i]
+
+    # update cells
+    ws.update_cells(cell_list)
 
 if __name__ == '__main__':
 
@@ -34,6 +55,7 @@ if __name__ == '__main__':
     u = os.environ['AC_USER']
     p = os.environ['AC_PASS']
     d = os.environ['AC_DOC']
-
-    d, k = fetch_from_google_docs(u, p, d)
-    print d
+    
+    data, k = fetch_from_google_docs(u, p, d)
+    
+    insert_into_google_docs(u, p, d, data)
